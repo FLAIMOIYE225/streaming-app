5ce83c8d71f5fbee04fc25ac32433f40528df799
KOFFI DEV <koffidevweb@gmail.com>
2025-10-22 10:23:40 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+import yt_dlp
+
+app = Flask(__name__)
+
+@app.route("/extract", methods=["GET"])
+def extract():
+    url = request.args.get("url")
+    ydl_opts = {'format': 'bestaudio/best', 'quiet': True, 'noplaylist': True}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return jsonify({
+            "title": info.get('title', ''),
+            "audio_url": info['url']
+        })
